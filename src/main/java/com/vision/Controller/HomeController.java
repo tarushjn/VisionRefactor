dd151459be973b2d6606d0eba4644050b303b144
Saurabh nagpal <saurabhnagpal@Saurabhs-MacBook-Air.local>
2017-07-13 11:40:37 +0530

    added index.jsp

--- src/main/java/com/vision/Controller/HomeController.java
+++ src/main/java/com/vision/Controller/HomeController.java
@@ -1,7 +1,15 @@
 package com.vision.Controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
 /**
  * Created by saurabhnagpal on 30/06/17.
  */
-public class HomeController {
+@Controller
+public class HomeController {@RequestMapping(value="/")
+public String index() {
+    return "index";
+
+    }
 }
